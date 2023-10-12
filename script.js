5f48ce5188d78dc078aa2e6e40d73ff952624460
nim1236 <147692828+nim1236@users.noreply.github.com>
2023-10-12 09:56:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Получаем элемент с id "counter"
+const counterElement = document.getElementById("counter");
+
+// Инициализируем переменную count
+let count = 0;
+
+// Функция для обновления значения счетчика на странице
+function updateCounter() {
+    counterElement.textContent = count;
+}
+
+// Функция для увеличения значения счетчика
+function incrementCounter() {
+    count++;
+    updateCounter();
+}
+
+// Функция для уменьшения значения счетчика
+function decrementCounter() {
+    count--;
+    updateCounter();
+}
+
+// Добавляем обработчик клика на кнопку "+"
+document.getElementById("plus-btn").addEventListener("click", incrementCounter);
+
+// Добавляем обработчик клика на кнопку "-"
+document.getElementById("minus-btn").addEventListener("click", decrementCounter);
+
+// Добавляем обработчик клика на "hw-1" в хедере
+document.getElementById("hw1").addEventListener("click", function() {
+    alert("Вы кликнули на hw-1 в хедере");
+});
+
+// Получаем элемент с id "homework-list"
+const homeworkList = document.getElementById("homework-list");
+
+// Функция для обработки клика на элементе homework
+function handleClickOnHomework() {
+    alert("Вы кликнули на элемент homework");
+}
+
+// Получаем все элементы с классом "homework"
+const homeworkItems = document.querySelectorAll(".homework");
+
+// Добавляем обработчик клика для каждого элемента homework
+homeworkItems.forEach(function(item) {
+    item.addEventListener("click", handleClickOnHomework);
+});
