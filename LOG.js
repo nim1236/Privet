cc347e8fe2c643ee117d46f7314e192e145124f8
nim1236 <147692828+nim1236@users.noreply.github.com>
2023-10-25 14:47:32 +0300

    Add files via upload

--- LOG.js
+++ LOG.js
@@ -22,8 +22,8 @@ function redirectToLogin() {
         alert("Введите пароль правильно.");
         return; // Прерываем выполнение функции, так как пароль не соответствует требованию
     }
-    document.cookie = "homeworkCookie=1";
     window.location.href = "LOGIN1.html";
+    document.cookie = "homeworkCookie=1";
     document.getElementById('toggle-password').checked = false;
 }
 var password = document.getElementById('password');
