92eb8149a20e5ebd8888e0d2fedc0e9ae0acd7ff
nim1236 <147692828+nim1236@users.noreply.github.com>
2023-10-25 18:58:32 +0300

    Add files via upload

--- LOG1.js
+++ LOG1.js
@@ -3,3 +3,10 @@ const finishButton = document.querySelector(".finish-button");
 finishButton.addEventListener("click", function() {
     window.location.href = "index.html";
 });
+
+const deleteStorageButton = document.querySelector(".delete-storage-button");
+
+deleteStorageButton.addEventListener("click", function() {
+    localStorage.removeItem('user');
+    alert("Сохранения удалены.");
+});
